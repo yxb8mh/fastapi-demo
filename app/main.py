e569e4fef0ffa6d38c00ab29e5bd6356c703d675
yxb8mh <157515847+yxb8mh@users.noreply.github.com>
2024-10-28 00:25:49 +0000

    edited main.py file

--- app/main.py
+++ app/main.py
@@ -19,3 +19,11 @@ def add(a: int, b: int):
 @app.get("/square/{a}")
 def square(a: int):
     return {"result": a * a}
+
+@app.get("/user/{user_id}")
+def get_user(user_id: int):
+    return {
+        "user_id": user_id,
+        "name": "Carrie Feng",
+        "email": "yxb8mh@virginia.edu"
+    }
